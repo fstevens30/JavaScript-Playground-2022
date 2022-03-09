5336c073a5440d9b1d6fe72c4337800236d5ade1
fstevens30 <flynn-stevens@live.nmit.ac.nz>
2022-03-09 13:56:03 +1300

    Added text to app.js

---
+++ src/app.js
@@ -0,0 +1,9 @@
+console.log("Hello World!")
+    //TODO: Need to do this.
+
+    //NOTE: That was my list.
+/* 
+Make sure to make an app.js in an src folder.
+Make sure to save.
+Then use command [node app.js] to view changes.
+*/
