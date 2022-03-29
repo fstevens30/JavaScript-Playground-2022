ece4ad102cd5dda001342cd3e3e187dd8a0a73a1
fstevens30 <flynn-stevens@live.nmit.ac.nz>
2022-03-30 10:24:30 +1300

    Added Console Commands

--- src/app.js
+++ src/app.js
@@ -3,3 +3,9 @@
 // NOTE- I'll use VS Code on my left and chrome on my right which has MDN, devtools and github open.
 // TODO- Have a look through GiHub student offers and sign up to software for the course.
 // NOTE- Javascript is a set of instructions for your website/app to follow.
+// NOTE- Console Commands-
+console.log("Hello World")
+console.dir(document)
+console.table({first:"test",val:9});
+console.error("Hello World")
+
