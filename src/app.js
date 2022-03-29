bbf1174889ace2cbd7797aae27f07c13e3876e62
fstevens30 <flynn-stevens@live.nmit.ac.nz>
2022-03-30 11:25:06 +1300

    Notes added

--- src/app.js
+++ src/app.js
@@ -1,9 +1,10 @@
-//Week 6 Java-Script
-//Today we are learining how to setup our workspace.
-// NOTE- I'll use VS Code on my left and chrome on my right which has MDN, devtools and github open.
-// TODO- Have a look through GiHub student offers and sign up to software for the course.
-// NOTE- Javascript is a set of instructions for your website/app to follow.
-// NOTE- Console Commands-
+/*Week 6 Java-Script
+Today we are learining how to setup our workspace.
+ NOTE- I'll use VS Code on my left and chrome on my right which has MDN, devtools and github open.
+ TODO- Have a look through GiHub student offers and sign up to software for the course.
+ NOTE- Javascript is a set of instructions for your website/app to follow.
+ NOTE- Console Commands-
+ */
 console.log("Hello World")
 //console.dir(document)
 console.table({first:"test",val:9});
@@ -26,13 +27,19 @@ console.log(myName);
 var myAge = 20;
 console.log(myAge);
 
-//This code declares two variables myName and myCourse.
-//Then prints the value of myName in console.
-//Then prints alert in the browser.
-//Then declares myAge variable.
-//Then prints the value of myAge in console.
+/*This code declares two variables myName and myCourse.
+Then prints the value of myName in console.
+Then prints alert in the browser.
+Then declares myAge variable.
+Then prints the value of myAge in console.
+*/
+/* NOTE- (;) Semicolon always ends the statement. It isn't needed for one line but when there is multiple you need to add them at the end of each line.
+ NOTE- To add JS to an HTML you can put the code in <script></script>
+ NOTE- Another option for this is <script src=".src/JS_File_Name.js"></script>
+ NOTE- It is reccomended to make seperate HTML and JS files and link them with the above HTML command.
+*/
+ /* You can write multiple line comments like this
+It won't be rendered out 
+*/
 
-// NOTE- (;) Semicolon always ends the statement. It isn't needed for one line but when there is multiple you need to add them at the end of each line.
-// NOTE- To add JS to an HTML you can put the code in <script></script>
-// NOTE- Another option for this is <script src=".src/JS_File_Name.js"></script>
-// NOTE- It is reccomended to make seperate HTML and JS files and link them with the above HTML command.
+
