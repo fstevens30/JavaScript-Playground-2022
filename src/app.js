a893148ea37319bb339b58621dc849a15c693f00
fstevens30 <flynn-stevens@live.nmit.ac.nz>
2022-03-30 10:47:34 +1300

    Added more code

--- src/app.js
+++ src/app.js
@@ -1,11 +1,27 @@
-# Week-6 Java-Script
-## Today we are learining how to setup our workspace.
+//Week 6 Java-Script
+//Today we are learining how to setup our workspace.
 // NOTE- I'll use VS Code on my left and chrome on my right which has MDN, devtools and github open.
 // TODO- Have a look through GiHub student offers and sign up to software for the course.
 // NOTE- Javascript is a set of instructions for your website/app to follow.
 // NOTE- Console Commands-
 console.log("Hello World")
-console.dir(document)
+//console.dir(document)
 console.table({first:"test",val:9});
 console.error("Hello World")
 
+// Lesson Challenge
+console.log("Hello World")
+var a = "This is my test code for the lesson challenge, I hope it works - Flynn"
+console.log(a)
+
+//window.alert() displays alerts in a new window example below
+//window.alert("Ooopsie Daisies")
+//alert("Hello world!")
+
+//Code Snippet (Alert is commented out as it won't run in VS Code)
+var myName = "Flynn";
+var myCourse = "JavaScript";
+console.log(myName);
+//alert("Welcome Traveller!");
+var myAge = 20;
+console.log(myAge);
