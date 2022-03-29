d3f70fc29d211c419556946307e6b40acc63e752
fstevens30 <flynn-stevens@live.nmit.ac.nz>
2022-03-30 10:01:22 +1300

    Added .html with code

--- src/app.js
+++ src/app.js
@@ -1,3 +1,5 @@
-#Week-6 Java-Script
-##Today we are learining how to setup our workspace.
+# Week-6 Java-Script
+## Today we are learining how to setup our workspace.
 // NOTE- I'll use VS Code on my left and chrome on my right which has MDN, devtools and github open.
+// TODO- Have a look through GiHub student offers and sign up to software for the course.
+// NOTE- Javascript is a set of instructions for your website/app to follow.
