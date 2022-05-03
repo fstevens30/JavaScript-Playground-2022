684e6c3e1cb8af47495798c46c6e1aa83f08d6ea
fstevens30 <flynn-stevens@live.nmit.ac.nz>
2022-05-04 10:39:51 +1200

    Task 3

--- src/app.js
+++ src/app.js
@@ -1,29 +1,29 @@
 // Week 9
-//Task 1
+//ANCHOR Task 1
 // Create a function that takes an array and returns both the minumum 
 // and maximum numbers in order 
 
-// function findMinMax(arr){ //declared function findMinMax with array as parameter.
-//     let array = []
-//     let min = arr[0]
-//     let max = arr[0]
-//     for (let i = 0; i < arr.length; i++) {
-//         if(arr[i] < min){
-//             min = arr[i]
-//         }
-//         if(arr[i] > max){
-//             max= arr[i]
-//         }
-//     }
-//     array.push(min)
-//     array.push(max)
+function findMinMax(arr){ //declared function findMinMax with array as parameter.
+    let array = []
+    let min = arr[0]
+    let max = arr[0]
+    for (let i = 0; i < arr.length; i++) {
+        if(arr[i] < min){
+            min = arr[i]
+        }
+        if(arr[i] > max){
+            max= arr[i]
+        }
+    }
+    array.push(min)
+    array.push(max)
 
-//     return array
-// }
+    return array
+}
 
-// console.log(findMinMax([1,2,3,4,5]))
+console.log(findMinMax([1,2,3,4,5]))
 
-//Task 2
+//ANCHOR Task 2
 //Create a function that concatentates a number of arrays together
 //Example arraysConcat([1,2,3,4,5], [6,7,8,9]) -> [1,2,3,4,5,6,7,8,9]
 
@@ -42,5 +42,16 @@
 // console.log(concatArray([1,2,3,4], [6,7,8,9]))
 
 // 2nd solution (INSANE)
-const concatArray = (...args) => args.flat(Infinity)
+const concatArray = (...a) => a.flat(Infinity)
 console.log(concatArray([1,2,3,4,5], [6,7,8,9]))
+
+//ANCHOR Task 3
+//Create a function that takes an array of numbers, a string
+//and return an array of numbers as per the following rules:
+//arrayRules([4,3,2,1], "Asc") -> [1,2,3,4]
+//arrayRules([5,6,7,8,9], "Des") -> [9,8,7,6,5]
+//arrayRules([4,3,2,1], "None") -> [4,3,2,1]
+
+function arrayRules([], string){
+    
+}
