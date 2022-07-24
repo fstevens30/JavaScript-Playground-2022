3ef70028561f117fde40c3398c10dfcc95ae67ae
fstevens30 <flynn-stevens@live.nmit.ac.nz>
2022-07-25 10:15:55 +1200

    Added Notes

--- roman-numeral-converter.js
+++ roman-numeral-converter.js
@@ -1,14 +1,14 @@
 //Roman Neumeral Converter
 function convertToRoman(num) {
-  var romanNumeral = "";
-  var romanNumeralArray = ["M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"];
-  var romanNumeralValue = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1];
-  for (var i = 0; i < romanNumeralValue.length; i++) {
-    while (num >= romanNumeralValue[i]) {
-      romanNumeral += romanNumeralArray[i];
-      num -= romanNumeralValue[i];
+  var romanNumeral = ""; //empty string
+  var romanNumeralArray = ["M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"]; //Array of Roman Numeral Strings
+  var romanNumeralValue = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1]; //create an array of roman numerals and their values
+  for (var i = 0; i < romanNumeralValue.length; i++) { //loop through the array of roman numerals and their values
+    while (num >= romanNumeralValue[i]) { //while the number is greater than the value of the roman numeral
+      romanNumeral += romanNumeralArray[i]; //add the roman numeral to the string
+      num -= romanNumeralValue[i]; //subtract the value of the roman numeral from the number
     }
   }
-  return romanNumeral;
+  return romanNumeral; //   return the string
 }
-console.log(convertToRoman(2));
+console.log(convertToRoman(2)); // should return "II"
