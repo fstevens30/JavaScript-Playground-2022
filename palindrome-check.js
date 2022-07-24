8a8ae108a64bfc7873a845b86fbbb6f3f4c25367
fstevens30 <flynn-stevens@live.nmit.ac.nz>
2022-07-25 09:35:45 +1200

    Finished checker

--- palindrome-check.js
+++ palindrome-check.js
@@ -3,11 +3,13 @@
 // A palindrome is a word that’s the same backwards and forwards.
 // For example, “racecar” is a palindrome.
 
+/*
 function palindromeCheck(str) {
   var reversed = str.split('').reverse().join('');
   return reversed === str;
 }   
 console.log(palindromeCheck('racecar'));
+*/
 
 //split() method 
 //The split() method splits a String object into an array of strings by separating the string into substrings.
@@ -29,3 +31,30 @@ console.log(reversed);
 const str3 = [["This"], ["is"], ["a"], ["string."]];
 const joined = str3.join(' ');
 console.log(joined);
+
+//remove spaces and format capital letters to lowerscase
+//The replace() method returns a new string with some or all matches of a pattern replaced by a replacement. 
+//The pattern can be a string or a RegExp, and the replacement can be a string or a function to be called for each match. 
+
+const str4 = "My name is Flynn, this is a test.";
+const formatted = str4.replace(/\s/g, '').toLowerCase();
+console.log(formatted);
+
+//Using Regex
+var regex = /[!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~]/g;
+
+function removePunctuation(string) {
+  return string.replace(regex, '');
+}
+console.log(removePunctuation('My name is Flynn, this is a test.'));
+
+//Final Solution
+
+function checker(str) {
+  var clean = str.replace(/[!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~]/g, '').toLowerCase().replace(/\s/g, '');
+  var reversed = clean.split('').reverse().join('');
+  return reversed === clean;
+}
+console.log(checker('A man, a plan, a canal. Panama'));
+
+
