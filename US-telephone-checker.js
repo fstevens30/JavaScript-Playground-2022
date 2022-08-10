beb2f8c8460132094054cb7931694671b7f2f89b
Flynn <flynn-stevens@live.nmit.ac.nz>
2022-08-10 12:42:47 +1200

    Added switch case

--- US-telephone-checker.js
+++ US-telephone-checker.js
@@ -1,3 +1,5 @@
+//My first attempt at a telephone checker.
+/*
 function checkTel(tel) {
     //First remove all non-numeric characters from the string.
     var tel = tel.replace(/[^a-zA-Z0-9]/g, '');
@@ -6,7 +8,7 @@ function checkTel(tel) {
         return true; //If the tel is 10 characters long return true.
     }
     else if (tel.length === 11) {
-        if(tel[0] === '1') { //If the first character is 1 return true.
+        if(tel[0] === "1") { //If the first character is 1 return true.
             return true;
         } else { //If the first character is not 1 return false.
             return false;
@@ -15,5 +17,71 @@ function checkTel(tel) {
         return false;
     }
 }
+*/
 
-console.log(checkTel("1-555-555-5555")); //Returns false
+/*
+//How it should work
+//First check the lenth of the string. If it is 10 or 11 characters long return keep going.
+
+//Then check if the first character is 1. If it is keep going.
+
+//Check length of a string ignoring all characters except numbers.
+function checkLength(str) {
+    var str = str.replace(/[^0-9]/g, '');
+    return str.length;
+}
+console.log(checkLength("Hello World")); //Should return 0
+
+//Check if the first character is 1.
+function checkFirst(str) {
+    if (str[0] === "1") {
+        return true;
+    } else {
+        return false;
+    }
+}
+console.log(checkFirst("Hello World")); //Should return false
+console.log(checkFirst("1Hello World")); //Should return true
+
+//Combine these two functions into one.
+function checkTel(str) {
+    if (checkTel(str[0] === 1)) {
+        var clean = str.replace(/[^0-9]/g, '');
+        if (clean.length === 10 || clean.length === 11) {
+            return true;
+        }
+    }
+    return false;
+}
+console.log(checkTel("Hello World")); //Should return false
+console.log(checkTel("-1 (555) 555 5555")); //Should return true
+*/
+function telephoneCheck(str){
+    var clean = str.replace(/[^0-9]/g, '');
+    if (clean.length === 10) {
+        return true;
+    }
+    else if (clean.length === 11 && str[0] === "1") {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+console.log(telephoneCheck("1-2222-111-2222")); //Should return false
+
+console.log(telephoneCheck("1 (555) 555-5555")); //Should return true
+
+//switch statement to check if the first character is 1.
+function telephoneCheck(str){
+    switch (str[0]) {
+        case "1":
+            var clean = str.replace(/[^0-9]/g, '');
+            if (clean.length === 10 || clean.length === 11) {
+                return true;
+            }
+            break;
+        default:
+            return false;
+    }
+}
