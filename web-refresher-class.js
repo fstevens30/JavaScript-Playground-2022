76344ee6d72ae611ba0ec7c556c755567d86af5a
Flynn <flynn-stevens@live.nmit.ac.nz>
2022-08-16 11:13:32 +1200

    Task one, convert to seconds

---
+++ web-refresher-class.js
@@ -0,0 +1,14 @@
+//A refresher activity for JavaScript in WEB503
+
+//ANCHOR Task one
+
+//create a function that takes an integer (minutes) and converts it to seconds
+
+function convertToSeconds(minutes) { //creating convertToSeconds function that takes an argument of minutes
+    return minutes * 60; //the function returns the minutes multiplied by 60
+}
+console.log(convertToSeconds(1));
+
+//ANCHOR Task two
+
+//
