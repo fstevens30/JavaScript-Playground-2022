2cf198282f95360634e735f76257f29f81f4ad92
Flynn <flynn-stevens@live.nmit.ac.nz>
2022-08-16 11:21:02 +1200

    Added task two ES5 & ES6

--- web-refresher-class.js
+++ web-refresher-class.js
@@ -16,4 +16,14 @@ console.log(convertSec(1));
 
 //ANCHOR Task two
 
-//
+// Create a function that takes an array (only numbers) and returns the first value.
+
+function getFirstValue(array) { //creating getFirstValue function that takes an argument of array
+    return array[0]; //the function returns the first value of the array
+}
+console.log(getFirstValue([1, 2, 3, 4, 5])); //calling the function and passing in an array
+
+//Refactoring to ES6
+
+const getFirst = (arr) => arr[0];
+console.log(getFirst([1, 2, 3, 4, 5]));
