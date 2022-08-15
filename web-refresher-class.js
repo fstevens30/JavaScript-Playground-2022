5b464dccff17e3e9d44c7b40af22f4ce33cf4791
Flynn <flynn-stevens@live.nmit.ac.nz>
2022-08-16 11:47:12 +1200

    Added SDV task

--- web-refresher-class.js
+++ web-refresher-class.js
@@ -27,3 +27,17 @@ console.log(getFirstValue([1, 2, 3, 4, 5])); //calling the function and passing 
 
 const getFirst = (arr) => arr[0];
 console.log(getFirst([1, 2, 3, 4, 5]));
+
+//ANCHOR Task for SDV503 students only
+
+// Create a function that takes an array of objects (computer items) which will calculate the total price of a built computer and return it as a number.
+
+
+function calculateTotalPrice(array) { //creating calculateTotalPrice function that takes an argument of array
+    let total = 0; //creating a variable total and setting it to 0
+    for (let i = 0; i < array.length; i++) { //creating a for loop that will run as long as the length of the array is greater than 0
+        total += array[i].price; //the total variable will be added to the price of the array
+    }
+    return total; //the function will return the total variable
+}
+console.log(calculateTotalPrice([{ "product": "Dell", "ram": 16, "price": 1200 }]));
