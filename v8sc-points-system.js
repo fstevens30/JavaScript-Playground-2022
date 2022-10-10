859278b3c7fba828f74679b154ba8e1e7dea36cb
Flynn Stevens <flynn-stevens@live.nmit.ac.nz>
2022-10-11 09:35:07 +1300

    Created the gis and made sydney r2

--- v8sc-points-system.js
+++ v8sc-points-system.js
@@ -13,7 +13,8 @@ class Race {
 //Create an array of races with availible points
 
 const races = [
-    new Race("Sydney", [150, 138, 129, 120, 111, 102, 96, 90, 84, 78, 72, 69, 66, 63, 60, 57, 54, 51, 48, 45, 42, 39, 36, 33, 30, 27, 24, 21, 18, 15]),
+    new Race("Sydney R1", [150, 138, 129, 120, 111, 102, 96, 90, 84, 78, 72, 69, 66, 63, 60, 57, 54, 51, 48, 45, 42, 39, 36, 33, 30, 27, 24, 21, 18, 15]),
+    new Race("Sydney R2", [150, 138, 129, 120, 111, 102, 96, 90, 84, 78, 72, 69, 66, 63, 60, 57, 54, 51, 48, 45, 42, 39, 36, 33, 30, 27, 24, 21, 18, 15]),
 ]
 
 //Create class for the drivers
@@ -31,3 +32,9 @@ class Driver {
         this.driverNumber = Number(driverNumber);
     }
 }
+
+//Create an array of drivers
+
+const drivers = [
+    new Driver("Shane", "van Gisbergen", "Red Bull Ampol Racing", 97),
+]
