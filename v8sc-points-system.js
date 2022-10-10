1353f6e652640853a2231eeca859c7a8cb07f6f7
Flynn Stevens <flynn-stevens@live.nmit.ac.nz>
2022-10-11 09:19:57 +1300

    Added Races class and constructor

--- v8sc-points-system.js
+++ v8sc-points-system.js
@@ -2,3 +2,14 @@
 //Using the points system from the 2022 season
 
 //create class for the races
+
+class Races {
+    constructor(raceName, [racepoints]) { // Constructor takes a variable of raceName and an array of points availible for the race
+        this.raceName = raceName;
+        this.racepoints = racepoints;
+    }
+
+    // Create the arrays of points for each race
+
+
+}
