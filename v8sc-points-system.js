e1fc3d62fdd629d3b670f075b9039455c7381a89
Flynn Stevens <flynn-stevens@live.nmit.ac.nz>
2022-10-11 09:28:33 +1300

    Created driver class

--- v8sc-points-system.js
+++ v8sc-points-system.js
@@ -15,3 +15,19 @@ class Race {
 const races = [
     new Race("Sydney", [150, 138, 129, 120, 111, 102, 96, 90, 84, 78, 72, 69, 66, 63, 60, 57, 54, 51, 48, 45, 42, 39, 36, 33, 30, 27, 24, 21, 18, 15]),
 ]
+
+//Create class for the drivers
+
+class Driver {
+    constructor(
+        driverFirstName,
+        driverLastName,
+        driverTeam,
+        driverNumber,
+    ) {
+        this.driverFirstName = String(driverFirstName);
+        this.driverLastName = String(driverLastName);
+        this.driverTeam = String(driverTeam);
+        this.driverNumber = Number(driverNumber);
+    }
+}
