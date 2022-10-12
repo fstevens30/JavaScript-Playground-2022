cf6fb065ef195fb3a6c22a50f4e0c7dcbe126da9
Flynn Stevens <flynn-stevens@live.nmit.ac.nz>
2022-10-13 11:10:12 +1300

    Added code

---
+++ confirm-ending.js
@@ -0,0 +1,7 @@
+function confirmEnding(str, target) { // function to check if a string ends with a target string takes two arguments string and target
+    return str.substr(-target.length) === target; // returns true if the last characters of the string match the target string
+}
+
+console.log(confirmEnding("Bastian", "n"));
+
+// The substr() method extracts parts of a string, beginning at the character at the specified position, and returns the specified number of characters.
