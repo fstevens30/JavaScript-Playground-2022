0c7d9fcbb0fe06674ac0968b024984f64d51ff34
Flynn Stevens <flynn-stevens@live.nmit.ac.nz>
2022-10-13 13:19:15 +1300

    Added code

--- FCC-Playground.js
+++ FCC-Playground.js
@@ -10,4 +10,18 @@ console.log(confirmEnding("Bastian", "n"));
 
 // The substr() method extracts parts of a string, beginning at the character at the specified position, and returns the specified number of characters.
 
-//
+// NOTE function that repeats a given string num times. If num is a negative number return an empty string
+
+// NOTE can be done using the .repeat() method but this task requires a different approach
+
+function repeatStringNumTimes(str, num) {
+    if (num < 0) {
+        return "";
+    } else {
+        let newStr = "";
+        for (let i = 0; i < num; i++) {
+            newStr += str;
+        }
+        return newStr;
+    }
+}
