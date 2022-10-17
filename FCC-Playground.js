0bebb316fc6f0eacc1bca7f2dd5fa1ed9bbf2e34
Flynn Stevens <flynn-stevens@live.nmit.ac.nz>
2022-10-17 14:39:44 +1300

    Fixed function and console.log()

--- FCC-Playground.js
+++ FCC-Playground.js
@@ -101,6 +101,7 @@ console.log(findLongestWordLength("The quick brown fox jumped over the lazy dog"
 // Example 
 // largestOfFour([[4, 5, 1, 3], [13, 27, 18, 26], [32, 35, 37, 39], [1000, 1001, 857, 1]]) should return [5, 27, 39, 1001]
 
+/*
 function largestOfFour(arr) { // function takes an array as an argument
     let largestNums = []; // variable to store the largest numbers
     for (let i = 0; i < arr.length; i++) { // loop through the array
@@ -121,3 +122,34 @@ console.log(largestOfFour([
     [32, 35, 37, 39],
     [1000, 1001, 857, 1]
 ])); // should return [5, 27, 39, 1001]
+
+*/
+
+// This works but doesn't pass the tests because of negative numbers 
+// Example largestOfFour([17, 23, 25, 12], [25, 7, 34, 48], [4, -10, 18, 21], [-72, -3, -17, -10]) should return [25, 48, 21, -3]
+
+// Instead of setting the largestNum to 0 set it to the first element of the sub array
+
+// Refarctoring the above function
+
+function largestOfFour(arr) {
+    const results = [];
+    for (let i = 0; i < arr.length; i++) {
+        let largestNumber = arr[i][0];
+        for (let j = 1; j < arr[i].length; j++) {
+            if (arr[i][j] > largestNumber) {
+                largestNumber = arr[i][j];
+            }
+        }
+        results[i] = largestNumber;
+    }
+
+    return results;
+}
+
+console.log(largestOfFour([
+    [17, 23, 25, 12],
+    [25, 7, 34, 48],
+    [4, -10, 18, 21],
+    [-72, -3, -17, -10]
+])); // should return [25, 48, 21, -3]
