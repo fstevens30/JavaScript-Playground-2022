e8d297194ec01587ed80d2071319d3ff72267be5
Flynn Stevens <flynn-stevens@live.nmit.ac.nz>
2022-10-27 13:58:26 +1300

    Added slider bar

--- progress-bar/progress.js
+++ progress-bar/progress.js
@@ -1,9 +1,30 @@
+let progressBar = document.getElementById('progressBar'); // Select the progress bar
+// Set a variable to the inputted number
+let progress = document.getElementById('progress').value;
+// Set the progress bar to the inputted number
+
 //When the function runs update the progress bar to the inputted number
 
 function updateProgress() {
-    var progressBar = document.getElementById('progressBar'); // Select the progress bar
-    // Set a variable to the inputted number
-    var progress = document.getElementById('progress').value;
-    // Set the progress bar to the inputted number
     progressBar.value = progress;
+}
+
+//Switch case for changing the bar colour when the value is different.
+
+switch (progress) {
+    case 0:
+        document.documentElement.style.setProperty('--progress-color', 'green');
+        break;
+    case 250:
+        document.documentElement.style.setProperty('--progress-color', 'yellow');
+        break;
+    case 500:
+        document.documentElement.style.setProperty('--progress-color', 'orange');
+        break;
+    case 750:
+        document.documentElement.style.setProperty('--progress-color', 'red');
+        break;
+    default:
+        document.documentElement.style.setProperty('--progress-color', 'green');
+        break;
 }
